473ecc15ec8bc24836944b09837d3ffbdbb26ccf
Nik Marchenko <nick.a.march@gmail.com>
2021-07-29 21:12:09 -0700

    Add simple connector

--- lib/build_connector.rb
+++ lib/build_connector.rb
@@ -1,5 +1,9 @@
 require 'rest-client'
 
-mapping_url = 'http://localhost:9200/test_index'
-res = RestClient.put(mapping_url, File.read('./lib/mapping.json'), {content_type: :json, accept: :json})
-puts res
+mapping_url = 'http://localhost:9200/test_topic'
+mapping_results = RestClient.put(mapping_url, File.read('./lib/mapping.json'), {content_type: :json, accept: :json})
+puts mapping_results
+
+connector_url = 'http://localhost:8083/connectors/test_topic/config'
+connector_result = RestClient.put(connector_url, File.read('./lib/connector.json'), {content_type: :json, accept: :json})
+puts connector_result
