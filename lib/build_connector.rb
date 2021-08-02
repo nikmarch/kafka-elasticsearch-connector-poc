62799ac3d3bbaf3c91ee613a2b3eda4d1ba1fb60
Nik Marchenko <nick.a.march@gmail.com>
2021-08-01 20:18:30 -0700

    WIP

--- lib/build_connector.rb
+++ lib/build_connector.rb
@@ -1,9 +1,28 @@
 require 'rest-client'
 
+topic = 'test_topic'
+
 mapping_url = 'http://localhost:9200/test_topic'
 mapping_results = RestClient.put(mapping_url, File.read('./lib/mapping.json'), {content_type: :json, accept: :json})
 puts mapping_results
 
 connector_url = 'http://localhost:8083/connectors/test_topic/config'
-connector_result = RestClient.put(connector_url, File.read('./lib/connector.json'), {content_type: :json, accept: :json})
+connector_body = {
+  "connector.class" => "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
+  "connection.url" => "elasticsearch:9200",
+  "read.timeout.ms" => 600000,
+  "tasks.max" => "1",
+  "type.name" => "_doc",
+  "key.converter" => "org.apache.kafka.connect.storage.StringConverter",
+  "value.converter" => "org.apache.kafka.connect.json.JsonConverter",
+  "value.converter.schemas.enable" => false,
+  "behavior.on.malformed.documents" => "warn",
+  "behavior.on.null.values" => "delete",
+  "drop.invalid.message" => true,
+  "topics" => topic
+}
+
+connector_result = RestClient.put(connector_url,
+                                  connector_body,
+                                  {content_type: :json, accept: :json})
 puts connector_result
