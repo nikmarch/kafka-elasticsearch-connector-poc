0a4394d4a60e9923e1863210f5d3a5273e61898e
Nik Marchenko <nick.a.march@gmail.com>
2021-08-05 21:21:29 -0700

    Moved customers.yaml to better place

--- lib/seed/test_cases.rb
+++ lib/seed/test_cases.rb
@@ -2,7 +2,7 @@ require 'yaml'
 require_relative '../kafka_producer.rb'
 
 pusher = KafkaProducer.new
-customers = YAML.load(File.read('./lib/customers.yaml'))
+customers = YAML.load(File.read('customers.yaml'))
 
 customers.map do |customer|
   customer.delete('test_cases')
