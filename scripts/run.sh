4abea41c22a27d25415b16a33a84076929b2f8d3
Nik Marchenko <nick.a.march@gmail.com>
2021-08-19 21:48:01 -0700

    Cleaner

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,5 +3,5 @@ echo -e "\nDestroy old docker environment\n" && \
   docker-compose down && \
   echo -e "\nBuild new docker environment\n" && \
   docker-compose up -d  --build && \
-  echo -e "\nRun data provisioners\n" && \
-  docker-compose exec rspec_container ./docker_run.sh
+  echo -e "\nRun data provisioners and test cases\n" && \
+  docker-compose exec rspec_container ./scripts/docker_run.sh
