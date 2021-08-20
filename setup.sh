c200cdb224726b23b0bde8c054e625b6f431716e
Nik Marchenko <nick.a.march@gmail.com>
2021-08-19 21:38:26 -0700

    Make it work

--- setup.sh
+++ setup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-echo -e "\Install gems for testing."
-bundle install
-
 echo -e "\nWaiting for Elasticsearch."
 until $(curl --output /dev/null --silent --head --fail http://elasticsearch:9200); do
     printf '.'
@@ -22,3 +19,9 @@ bundle exec ruby lib/seed/fake_cases.rb
 
 echo -e "\nSetup custom mapping and kafka connector."
 bundle exec ruby lib/build_connector.rb
+
+echo -e "\nWait for connecor to transfer data to Elasticsearch\n"
+sleep 30
+
+echo -e "\nRun searching test cases\n"
+bundle exec rspec
--- setup_and_run.sh
+++ setup_and_run.sh
@@ -2,10 +2,6 @@
 echo -e "\nDestroy old docker environment\n" && \
   docker-compose down && \
   echo -e "\nBuild new docker environment\n" && \
-  docker-compose up -d
-  # echo -e "\nRun data provisioners\n" && \
-  # ./setup.sh && \
-  # echo -e "\nWait for connecor to transfer data to Elasticsearch\n" && \
-  # sleep 30 && \
-  # echo -e "\nRun searching test cases\n" && \
-  # bundle exec rspec
+  docker-compose up -d  --build && \
+  echo -e "\nRun data provisioners\n" && \
+  docker-compose exec rspec ./setup.sh
